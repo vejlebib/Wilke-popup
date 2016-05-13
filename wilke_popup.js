5fe0a177981ae7e85db05e28e39dfa7cae9ae160
Simon Holt <simonholt83@gmail.com>
2016-05-13 16:40:36 +0200

    Initial commit

---
+++ wilke_popup.js
@@ -0,0 +1,7 @@
+(function($) {
+
+  $(document).ready(function() {
+    window.open('http://survey4.wilkeonline.com/entry.aspx?ssid=6371-98751&lan=da','wilkeSurvey6453746498','width=1000,height=700,toolbar=0,menubar=0,location=0,status=0,scrollbars=1,resizable=1,left=0,top=0');
+  });
+
+}(jQuery));
