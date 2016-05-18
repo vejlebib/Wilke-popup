2762faa1dd6660dd53323eb1755b6c27fba1939b
Simon Holt <simonholt83@gmail.com>
2016-05-18 16:20:19 +0200

    Set cookie to expire in 60 days

--- wilke_popup.js
+++ wilke_popup.js
@@ -7,7 +7,7 @@
           var wilke_popup = $("#wilke-popup", context);
           if (wilke_popup.length > 0) {
             $("#wilke-popup")[0].click();
-            $.cookie('wilke_popup_shown', 1);
+            $.cookie('wilke_popup_shown', 1, {"expires": 60});
           }
         }
     }
